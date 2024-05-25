6dfd342436e06826d4b6396e13005bb94b100261
Pingis2 <anton@worksit.se>
2024-05-25 19:53:20 +0200

    added loading animation

--- AntonSchyberg-portfolio/main.js
+++ AntonSchyberg-portfolio/main.js
@@ -1,1 +1,14 @@
 import '../css/main.css'
+
+const loaderContainer = document.getElementById('loader-container');
+const spinner = document.querySelector(".spinner")
+
+window.addEventListener('load', () => {
+    setTimeout(() => {
+        spinner.classList.add("hidden");
+        setTimeout(() => {
+            loaderContainer.style.animation = "shrink-container 1s linear forwards";
+        }, 800);
+    }, 200);
+    
+});
