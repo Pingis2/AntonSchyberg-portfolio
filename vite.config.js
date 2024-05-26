460b17ef79fd459d80ba89f2a3c2214a4c9ece19
Pingis2 <anton@worksit.se>
2024-05-26 07:37:59 +0200

    changed base to repo

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite"
 
 export default defineConfig ({
-    'base': 'https://pingis2.github.io/AntonSchyberg-portfolio/'
+    'AntonSchyberg-portfolio': 'https://pingis2.github.io/AntonSchyberg-portfolio/'
   });
