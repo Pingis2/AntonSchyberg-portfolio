cd7e353f02114b78db71861175a690fd26e80eee
Pingis2 <anton@worksit.se>
2024-05-26 07:56:42 +0200

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite"
 
 export default defineConfig ({
-    '/<AntonSchyberg-portfolio>/': 'https://pingis2.github.io/AntonSchyberg-portfolio/'
+    'base': 'https://pingis2.github.io/AntonSchyberg-portfolio/'
   });
