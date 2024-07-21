11d3566b312e5a94405b0d1d1047f1e562dd97c7
Pingis2 <anton@worksit.se>
2024-07-21 17:34:55 +0200

    updated burger menu

--- burger-menu/burger-menu.js
+++ burger-menu/burger-menu.js
@@ -1,42 +1,17 @@
 const burgerMenu = document.getElementById('burger');
 const burgerContent = document.getElementById('burgerContent');
-const burgerCurved = document.getElementById('burgerCurved');
+const burgerClose = document.getElementById('burgerClose');
 
 function openBurger() {
     burgerMenu.addEventListener('click', () => {
         burgerContent.classList.toggle('hidden');
+        burgerMenu.style.display = 'none';
     })
 
-    burgerCurved.addEventListener('click', () => {
+    burgerClose.addEventListener('click', () => {
         burgerContent.classList.toggle('hidden');
+        burgerMenu.style.display = "flex";
     })
 }
 
 openBurger();
-
-function checkScroll() {
-    const scrollPosition = window.scrollY;
-    const windowHeight = + window.innerHeight;
-    const documentHeight = document.documentElement.scrollHeight;
-    const scrollPercentage = (scrollPosition / (documentHeight - windowHeight)) * 100;
-    const windowWidth = window.innerWidth;
-
-    if (windowWidth > 344) {
-        if (scrollPercentage >= 15) {
-            burgerMenu.style.display = 'none';
-            burgerCurved.style.display = 'flex';
-            burgerCurved.style.justifyContent = 'flex-end';
-        } else { 
-            burgerMenu.style.display = 'flex';
-            burgerCurved.style.display = 'none';
-        }
-    } else {
-        burgerMenu.style.display = 'flex';
-        burgerCurved.classList.add('hidden');
-    }
-    
-}
-
-window.addEventListener('scroll', checkScroll);
-
-checkScroll();
