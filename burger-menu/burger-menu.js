07c6636f5526924b92f6b516d9e914b97521888a
Pingis2 <anton@worksit.se>
2024-07-23 15:54:58 +0200

    updated stars, updated burger menu and updated "other projects" page

--- burger-menu/burger-menu.js
+++ burger-menu/burger-menu.js
@@ -2,6 +2,8 @@ const burgerMenu = document.getElementById('burger');
 const burgerContent = document.getElementById('burgerContent');
 const burgerClose = document.getElementById('burgerClose');
 
+hideBurgerOnMobile();
+
 function openBurger() {
     burgerMenu.addEventListener('click', () => {
         burgerContent.classList.toggle('open');
@@ -11,5 +13,21 @@ function openBurger() {
         burgerContent.classList.toggle('open');
     })
 }
-
 openBurger();
+
+function hideBurgerOnMobile() {
+    if (sessionStorage.getItem('visibilityState') === 'gridpainter') {
+        burgerMenu.style.display = 'none';
+    } else if (sessionStorage.getItem('visibilityState') === 'isabellasSurfClub') {
+        burgerMenu.style.display = 'none';
+    } else if (sessionStorage.getItem('visibilityState') === 'swedishquizgame') {
+        burgerMenu.style.display = 'none';
+    } else if (sessionStorage.getItem('visibilityState') === 'documentCreator') {
+        burgerMenu.style.display = 'none';
+    } else if (sessionStorage.getItem('visibilityState') === 'donutWebshop') {
+        burgerMenu.style.display = 'none';
+    } else {
+        burgerMenu.style.display = 'flex';
+    }
+}
+
